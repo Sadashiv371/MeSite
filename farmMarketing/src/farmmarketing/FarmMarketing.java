768a74019bf1cec4153abdb31933cf682db7907d
Sadashiva Oram <62008748+Sadashiv371@users.noreply.github.com>
2023-07-28 22:13:15 +0530

    Add files via upload

---
+++ farmMarketing/src/farmmarketing/FarmMarketing.java
@@ -0,0 +1,17 @@
+
+package farmmarketing;
+
+/**
+ *
+ * @author Diwakar Giri
+ */
+public class FarmMarketing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
